565fef8b60921756a9111bf9a83caff94ded03f3
Alex Teixeira da Fonseca <alexatf2014@gmail.com>
2024-07-31 22:07:09 -0300

    feat(components) creates header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+
+// icons
+import HearthIcon from "../../public/icons/hearth.svg";
+import SearchIcon from "../../public/icons/search.svg";
+
+export function Header() {
+  return (
+    <header className="border-b border-line">
+      <div className="flex items-center justify-between pl-6">
+        <form className="flex w-[668px] items-center justify-between rounded border border-line px-4 py-2 transition-all hover:border-primary">
+          <input
+            type="text"
+            placeholder="Buscar usuário"
+            className="flex-1 border-none text-sm text-greyDark outline-none"
+          />
+          <button className="transition-all hover:brightness-90">
+            <Image src={SearchIcon} alt={"Ícone de pesquisa"} />
+          </button>
+        </form>
+
+        <Link
+          href={"/favorites"}
+          className="flex items-center gap-2 bg-primary px-6 py-7 text-sm text-white transition-all hover:brightness-90"
+        >
+          <Image src={HearthIcon} alt={"Ícone de coração"} />
+          Favoritos
+        </Link>
+      </div>
+    </header>
+  );
+}
--- src/layout/searchLayout.tsx
+++ src/layout/searchLayout.tsx
@@ -1,7 +1,14 @@
+import { Header } from "@/components/Header";
+
 interface ISearchLayoutProps {
   children: React.ReactElement;
 }
 
 export function SearchLayout({ children }: ISearchLayoutProps) {
-  return <div className="flex h-screen flex-col">{children}</div>;
+  return (
+    <div className="flex h-screen flex-col">
+      <Header />
+      {children}
+    </div>
+  );
 }
